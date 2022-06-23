523d6498bd943586f4cd02ea261016f4b7a365dd
Mandeep Singh <mandeep_singh1@softprodigy.com>
2022-06-23 17:12:20 +0530

    Added Broti and GZIp compression and Using Statements.

--- BlazorChat/Server/Program.cs
+++ BlazorChat/Server/Program.cs
@@ -1,14 +1,30 @@
-﻿using BlazorChat.Server;
-using Microsoft.AspNetCore.Builder;
+﻿
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.SmallestSize;
+});
 
 var startup = new Startup(builder.Configuration);
 
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
+app.UseResponseCompression();
 startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ BlazorChat/Server/Usings.cs
@@ -0,0 +1,6 @@
+﻿global using BlazorChat.Server;
+global using Microsoft.AspNetCore.Builder;
+global
+using Microsoft.AspNetCore.ResponseCompression;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.IO.Compression;
